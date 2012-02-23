58f51f2cc276c4c62411748ed476f724abcefb15
Prashant Sahni <prashant.sahni5@gmail.com>
2012-02-23 15:38:28 +0530

    -> Initial Commit :)

---
+++ app/controllers/testing_controller.rb
@@ -0,0 +1,9 @@
+class TestingController < ApplicationController
+
+  def index
+  end
+
+  def test
+   render :partial => 'test', :status => 200
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestingApp::Application.config.secret_token = '039268e70a7908cb0208288aa3f4f18e614cbe9bc70f72e887edd47731690e4eb022f636cfb9b8897dcb8c31c5e9ca1bc006e88da7a79f999967ee3cf9ed320d'
