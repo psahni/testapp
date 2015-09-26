1f9e0253384cc189cd4caaf9bf646b4fa1198948
Prashant Sahni <prashantkumar@modaoperandi.com>
2015-09-26 19:21:40 +0530

    #1 STI with polymorphic relation - finding child class type records from polymorphic model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,35 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true, :touch => true
 
   validates :message, :presence => true
+
+
+  scope :article_comments, ->{
+    joins("INNER JOIN articles ON articles.id=comments.commentable_id AND commentable_type='Article'")
+    .where('articles.type IS NULL')
+  }
+  
+  scope :special_article_comments, ->{ 
+                                  joins("INNER JOIN articles ON articles.id=comments.commentable_id AND commentable_type='Article'")
+                                  .where('articles.type =?', 'SpecialArticle') 
+  }
+
+#-------------------------------------------------------------------------------------------------------------------
+  #
+  # FOR ARTICLE AND ITS CHILD CLASSES THE COMMENTABLE TYPE WILL ALWAYS BE ARTICLE
+  #
+  # NOT RECOMMENDED - IF WE WANT TO SAVE COMMENTS WITH COMMENTABLE TYPE -> SPECIALARTICLE. AND THIS BREAK DESIGN.
+  #
+  # def commentable_type=(class_name)    
+  #   super(class_name)
+  # end
+#-------------------------------------------------------------------------------------------------------------------  
+  #
+  # FETCH ONLY CHILD CLASS COMMENTS :: SpecialArticle < Article
+  #
+  # def special_articles_comments
+  #   Comment.joins("INNER JOIN articles ON articles.id=comments.commentable_id AND commentable_type='Article'")
+  #   .where('articles.type =?', 'SpecialArticle')
+  # end
+
+  
 end
--- app/models/special_article.rb
+++ app/models/special_article.rb
@@ -1,2 +1,4 @@
-class SpecialArticle
+class SpecialArticle < Article
+  #self.inheritance_column = :my_custom_column
+  # TO BE USED IF TYPE COLUMN IS NOT USED  
 end
